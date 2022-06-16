b24af0f70a372dee54246d6ccff4bbfc04679d87
beyza-akcinar <93255363+beyza-akcinar@users.noreply.github.com>
2022-06-16 23:42:25 +0300

    REST

---
+++ Rest2/src/main/java/com/example/rest2/Rest2Application.java
@@ -0,0 +1,34 @@
+package com.example.rest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Rest2Application {
+    private static final Logger log = LoggerFactory.getLogger(Rest2Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rest2Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://quoters.apps.pcfone.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
+
+}
